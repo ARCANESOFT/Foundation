ebd33a4727b831e731d1e8bc25b880ee69b8f5c2
ARCANEDEV <arcanedev.maroc@gmail.com>
2015-11-23 16:06:04 +0000

    Updating gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -5,11 +5,13 @@
  * --------------------------------------------------------------------------
  */
 var gulp       = require('gulp'),
-    less       = require('gulp-less'),
     concat     = require('gulp-concat'),
+    less       = require('gulp-less'),
     minifyCss  = require('gulp-minify-css'),
+    notify     = require('gulp-notify'),
+    rename     = require('gulp-rename'),
     uglify     = require('gulp-uglify'),
-    notify     = require('gulp-notify');
+    merge      = require('merge-stream');
 
 /* --------------------------------------------------------------------------
  *  Directories
@@ -18,11 +20,83 @@ var gulp       = require('gulp'),
 var dirs       = {};
 dirs.resources = './resources/assets';
 dirs.bower     = dirs.resources + '/bower';
+dirs.dist      = dirs.resources + '/dist';
 
 /* --------------------------------------------------------------------------
  *  Main Tasks
  * --------------------------------------------------------------------------
  */
-gulp.task('foundation', ['foundation-less', 'foundation-js']);
+gulp.task('all',     ['default', 'vendors']);
+gulp.task('default', ['less', 'js']);
+gulp.task('vendors', ['css-vendors', 'js-vendors', 'img-vendors', 'fonts-vendors']);
 
-gulp.task('foundation-all', ['foundation', 'foundation-vendors']);
+/* --------------------------------------------------------------------------
+ *  Resources Tasks
+ * --------------------------------------------------------------------------
+ */
+gulp.task('less', function () {
+    return gulp.src(dirs.resources + '/less/style.less')
+        .on('error', notify.onError({
+            title:   'Error compiling LESS.',
+            message: 'Error: <%= error.message %>',
+            onLast:  true,
+            sound:   true
+        }))
+        .pipe(less())
+        .pipe(gulp.dest(dirs.dist + '/css'))
+        .pipe(minifyCss({
+            keepSpecialComments: 0
+        }))
+        .pipe(rename({ extname: '.min.css' }))
+        .pipe(gulp.dest(dirs.dist + '/css/'))
+        .pipe(notify({
+            title:   'Less compiled',
+            message: 'Less compiled with success !',
+            onLast:   true,
+            sound:    false
+        }));
+});
+
+gulp.task('js', function () {
+    return gulp.src(dirs.src + '/js/app.js')
+        .on('error', notify.onError({
+            title:   'Error compiling Javascript.',
+            message: 'Error: <%= error.message %>',
+            onLast:  true,
+            sound:   true
+        }))
+        .pipe(gulp.dest(dirs.dist + '/js'))
+        .pipe(uglify())
+        .pipe(rename({ extname: '.min.js' }))
+        .pipe(gulp.dest(dirs.dist + '/js/'))
+        .pipe(notify({
+            title:   'Javascript compiled',
+            message: 'Javascript compiled with success !',
+            onLast:   true,
+            sound:    false
+        }));
+});
+
+gulp.task('css-vendors', function () {
+    return gulp.src([
+        dirs.bower + '/foundation-assets/dist/css/*'
+    ]).pipe(gulp.dest(dirs.dist + '/css'))
+});
+
+gulp.task('js-vendors', function() {
+    return gulp.src([
+        dirs.bower + '/foundation-assets/dist/js/**/*'
+    ]).pipe(gulp.dest(dirs.dist + '/js'))
+});
+
+gulp.task('img-vendors', function() {
+    return gulp.src([
+        dirs.bower + '/foundation-assets/dist/img/*'
+    ]).pipe(gulp.dest(dirs.dist + '/img'))
+});
+
+gulp.task('fonts-vendors', function() {
+    return gulp.src([
+        dirs.bower + '/foundation-assets/dist/fonts/*'
+    ]).pipe(gulp.dest(dirs.dist + '/fonts'))
+});
