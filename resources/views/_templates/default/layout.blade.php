9e4ff627e3004df1eecd0f2ffe9b0649598b0151
ARCANEDEV <arcanedev.maroc@gmail.com>
2015-12-01 11:47:12 +0000

    Adding pace plugin

--- resources/views/_templates/default/layout.blade.php
+++ resources/views/_templates/default/layout.blade.php
@@ -7,6 +7,7 @@
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     {!! Html::style('vendor/foundation/css/foundation.min.css') !!}
     {!! Html::style('vendor/foundation/css/style.min.css') !!}
+    {!! Html::script('vendor/foundation/js/plugins/pace.min.js') !!}
     <!--[if lt IE 9]>
         {!! Html::script('https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js') !!}
         {!! Html::script('https://oss.maxcdn.com/respond/1.4.2/respond.min.js') !!}
