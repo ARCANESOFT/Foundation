81e1a190a5127a85d6eaeaf4929adc17b70811b8
ARCANEDEV <arcanedev.maroc@gmail.com>
2016-01-11 14:58:14 +0000

    Updating the setup console command

--- src/Console/SetupCommand.php
+++ src/Console/SetupCommand.php
@@ -37,14 +37,27 @@ class SetupCommand extends Command
      */
     public function handle()
     {
-        $arguments = [
+        if ( ! $this->confirm('Do you wish to reset the application ? [y|N]')) {
+            return;
+        }
+
+        $options = $this->getDefaultOptions();
+
+        $this->publishAllModules($options);
+        $this->setupAllModules($options);
+        $this->seedFoundation($options);
+    }
+
+    /**
+     * Get the default options.
+     *
+     * @return array
+     */
+    private function getDefaultOptions()
+    {
+        return $arguments = [
             '--quiet' => true,
         ];
-
-        if ($this->confirm('Do you wish to reset the application ? [y|N]')) {
-            $this->publishAllModules($arguments);
-            $this->setupAllModules($arguments);
-        }
     }
 
     /* ------------------------------------------------------------------------------------------------
@@ -54,11 +67,11 @@ class SetupCommand extends Command
     /**
      * Publish all modules : configs, migrations, assets ...
      *
-     * @param  array  $arguments
+     * @param  array  $options
      */
-    private function publishAllModules($arguments)
+    private function publishAllModules(array $options)
     {
-        $this->call('foundation:publish', $arguments);
+        $this->call('foundation:publish', $options);
 
         $this->call('optimize');
 
@@ -68,13 +81,23 @@ class SetupCommand extends Command
     /**
      * Setup all modules.
      *
-     * @param  array  $arguments
+     * @param  array  $options
      */
-    private function setupAllModules($arguments)
+    private function setupAllModules(array $options)
     {
-        $this->call('migrate:refresh');
+        $this->call('migrate:refresh', $options);
 
         // Setup the auth module.
-        $this->call('auth:setup', $arguments);
+        $this->call('auth:setup', $options);
+    }
+
+    /**
+     * Seed Foundation.
+     */
+    private function seedFoundation(array $options)
+    {
+        $this->call('db:seed', array_merge($options, [
+            '--class' => \Arcanesoft\Foundation\Seeds\DatabaseSeeder::class
+        ]));
     }
 }
