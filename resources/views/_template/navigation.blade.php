797083cf3751e1df40dbfe9596af37f40c4e37fe
ARCANEDEV <arcanedev.maroc@gmail.com>
2016-01-28 16:02:21 +0000

    Updating the navigation view

--- resources/views/_template/navigation.blade.php
+++ resources/views/_template/navigation.blade.php
@@ -1,13 +1,18 @@
 {{-- Header Navbar --}}
 <nav class="navbar navbar-static-top" role="navigation">
     {{-- Sidebar toggle button --}}
-    <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
+    <a href="javascript:void(0);" class="sidebar-toggle" data-toggle="offcanvas" role="button">
         <span class="sr-only">Toggle navigation</span>
     </a>
 
     {{-- Navbar Right Menu --}}
     <div class="navbar-custom-menu">
         <ul class="nav navbar-nav">
+            <li>
+                <a href="{{ route('public::home') }}" target="_blank">
+                    <i class="fa fa-fw fa-home"></i>
+                </a>
+            </li>
             {{-- Messages --}}
             @include('foundation::_template.navigation.messages')
             {{-- Notifications --}}
