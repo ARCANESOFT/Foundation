35e17451e3472721eaf87b467317221e6bba2b2d
ARCANEDEV <arcanedev.maroc@gmail.com>
2016-09-27 17:04:24 +0100

    Updating Controllers

---
+++ src/Http/Controllers/Controller.php
@@ -0,0 +1,14 @@
+<?php namespace Arcanesoft\Foundation\Http\Controllers;
+
+use Arcanesoft\Core\Bases\FoundationController;
+
+/**
+ * Class     Controller
+ *
+ * @package  Arcanesoft\Foundation\Http\Controllers
+ * @author   ARCANEDEV <arcanedev.maroc@gmail.com>
+ */
+abstract class Controller extends FoundationController
+{
+    //
+}
