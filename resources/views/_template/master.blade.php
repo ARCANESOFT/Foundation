240baf49d4e06307fb6809a50f99462811a76552
ARCANEDEV <arcanedev.maroc@gmail.com>
2016-10-27 18:24:41 +0100

    Updating the master layout

--- resources/views/_template/master.blade.php
+++ resources/views/_template/master.blade.php
@@ -1,15 +1,14 @@
 <!DOCTYPE html>
-<html>
+<html lang="{{ app()->getLocale() }}">
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     {!! seo_helper()->render() !!}
-    {!! Html::style('vendor/foundation/css/foundation.min.css') !!}
-    {!! Html::style('vendor/foundation/css/style.min.css') !!}
-    {!! Html::script('vendor/foundation/js/plugins/pace.min.js') !!}
+    {{ Html::style('vendor/foundation/css/app.css') }}
+    {{ Html::script('vendor/foundation/js/vendors/pace.min.js') }}
     <!--[if lt IE 9]>
-    {!! Html::script('https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js') !!}
-    {!! Html::script('https://oss.maxcdn.com/respond/1.4.2/respond.min.js') !!}
+    {{ Html::script('https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js') }}
+    {{ Html::script('https://oss.maxcdn.com/respond/1.4.2/respond.min.js') }}
     <![endif]-->
 </head>
 <body class="fixed sidebar-mini skin-purple hold-transition">
@@ -18,7 +17,7 @@
 
         @include('foundation::_template.sidebar-main')
 
-            <!-- Content Wrapper. Contains page content -->
+        {{-- Content Wrapper. Contains page content --}}
         <main class="content-wrapper">
             {{-- Content Header (Page header) --}}
             <section class="content-header">
@@ -43,9 +42,8 @@
         @include('foundation::_template.sidebar-alt')
     </div>
 
-    {!! Html::script('vendor/foundation/js/vendors.min.js') !!}
-    {!! Html::script('vendor/foundation/js/foundation.min.js') !!}
-    {!! Html::script('vendor/foundation/js/app.min.js') !!}
+    {{ Html::script('vendor/foundation/js/vendors.js') }}
+    {{ Html::script('vendor/foundation/js/app.js') }}
     @yield('scripts')
 </body>
 </html>
