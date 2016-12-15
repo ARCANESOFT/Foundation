71c1d9d7a66dfaa15904d4df3a29ee7d7d94ab70
ARCANEDEV <arcanedev.maroc@gmail.com>
2016-12-15 18:49:46 +0000

    Fixing the csrf token

--- resources/views/_template/csrf-token.blade.php
+++ resources/views/_template/csrf-token.blade.php
@@ -1,5 +1,5 @@
 <?php $token = csrf_token() ?>
 <meta name="csrf-token" content="{{ $token }}">
 <script>
-    window.Laravel = {!! json_encode(['csrfToken' => $token]) !!}
+    window.App = {!! json_encode(['csrfToken' => $token]) !!}
 </script>
